a3734fe077c40a8a91801afb6b8623a79b2d4738
jchj108 <jchj108@naver.com>
2021-08-23 14:31:56 +0900

    버블 정렬

--- Algorithm/src/sort/BubbleSort.java
+++ Algorithm/src/sort/BubbleSort.java
@@ -1,7 +1,32 @@
 package sort;
 
 public class BubbleSort {
+	
+	public void sort(int[] arr) {
+		
+		for(int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				int temp;
+				if(arr[j] <  arr[i]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	
+	public void swap()
+	
+	
 	public static void main(String[] args) {
 		
+		int[] arr = new int[] {3, 2, 5, 6, 1}; 
+		
+		new BubbleSort().sort(arr);
+		
+		for(int i : arr) {
+			System.out.println(i);
+		}
 	}
 }
