e613382edaa2123d26af12c542eef18f23573f55
jchj108 <jchj108@naver.com>
2021-09-10 21:22:59 +0900

    벌집 - 등비수열

---
+++ BaekJoon/src/level/Math1/Q1193_FindFraction.java
@@ -0,0 +1,23 @@
+package level.Math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1193_FindFraction {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		
+		// 분모 분자 값의 합 T
+		// 대각선 칸의 개수는 T-1
+		
+		//
+		
+		
+		
+	}
+
+}
---
+++ BaekJoon/src/level/Math1/Q2292_honeyComb.java
@@ -0,0 +1,28 @@
+package level.Math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2292_honeyComb {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int endPoint = 1;
+		for (int i = 1; i < Math.sqrt(1000000000); i++) {
+			endPoint += 6 * i;
+			if (N <= endPoint) {
+				if (N == 1) {
+					System.out.println(i);
+				} else {
+					System.out.println(i + 1);
+
+				}
+				break;
+			}
+		}
+	}
+}
