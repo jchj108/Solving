32ed99c5412997c7174a37209bb17523f9acf317
jchj108 <jchj108@naver.com>
2021-09-20 16:35:45 +0900

    달팽이는 올라가고 싶다

--- BaekJoon/src/level/Math1/Q2839_delivery.java
+++ BaekJoon/src/level/Math1/Q2839_delivery.java
@@ -7,30 +7,30 @@ import java.io.InputStreamReader;
 public class Q2839_delivery {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
+
 		int count = -1;
 		int remainder = 0;
-		
-		for(int i = 1; i < N/3; i++) {
-			if((N-(5*i))%3==0 && (N-(5*i) > 0)) {
-				remainder = N-(5*i) ;
-				count = remainder/3 + i;
+
+		for (int i = 1; i < N / 3; i++) {
+			if ((N - (5 * i)) % 3 == 0 && (N - (5 * i) > 0)) {
+				remainder = N - (5 * i);
+				count = remainder / 3 + i;
 			}
 		}
-		
-		if(count == -1 && N % 3 ==0) {
-			count = N/3;
+
+		if (count == -1 && N % 3 == 0) {
+			count = N / 3;
 		}
-		
-		if(N % 5 == 0) {
-			count = N/5;
+
+		if (N % 5 == 0) {
+			count = N / 5;
 		}
-		
+
 		System.out.println(count);
 	}
 }
---
+++ BaekJoon/src/level/Math1/Q2869_Snail.java
@@ -0,0 +1,29 @@
+package level.Math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2869_Snail {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		
+		if(V != A) {
+			if((V-A)%(A-B) >= 1) { 
+				System.out.println((V-A)/(A-B)+2);
+			} else { 
+				System.out.println((V-A)/(A-B)+1);
+			}
+		} else {
+			System.out.println(1);
+		}
+	}
+}
