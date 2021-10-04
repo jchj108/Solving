17cff93b13143f7c75df712abf094357480545a6
jchj108 <jchj108@naver.com>
2021-10-04 17:11:09 +0900

    도비의 영어 공부

---
+++ BaekJoon/src/guides/begginer/Q2386_Doby.java
@@ -0,0 +1,33 @@
+package guides.begginer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Q2386_Doby {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		char alp;
+		while(true) {
+			String st = br.readLine().toLowerCase();
+			int count = 0;
+			if(st.equals("#")) {
+				break;
+			}
+			alp = st.charAt(0);
+			for(int i = 2; i < st.length(); i++) {
+				if(st.charAt(i) == alp) {
+					count++;
+				}
+			}
+			bw.write(alp + " " + count+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
--- BaekJoon/src/level/QueueAndDeck/Q18258_Queue2.java
+++ BaekJoon/src/level/QueueAndDeck/Q18258_Queue2.java
@@ -1,11 +1,67 @@
 package level.QueueAndDeck;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q18258_Queue2 {
 
-	public static void main(String[] args) {
+	private static int size;
+	private static int num;
+	private static int[] que;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			switch(st.nextToken()) {
+			case "push" : push(Integer.parseInt(st.nextToken())); break;
+			case "pop" : pop(); break;
+			case "size" : size(); break;
+			case "empty" : empty(); break;
+			case "front" : front(); break;
+			case "back" : back(); break;
+			
+			}
+			
+		}
 		
 	}
+
+	private static void push(int n) {
+		que[back] = n;
+	}
+	private static void back() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void empty() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void size() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void front() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
