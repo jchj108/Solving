21e02d285c8b368846f1b11cb767afe82b378487
jchj108 <jchj108@naver.com>
2021-10-06 21:51:32 +0900

    N개의 최소공배수

---
+++ Programmers/src/level2/test/LeastCommonMultiple.java
@@ -0,0 +1,22 @@
+package level2.test;
+
+public class LeastCommonMultiple {
+	
+    public int solution(int[] arr) {
+    	
+    	// 최소 공배수 = 두 수의 곱 / 두 수의 최대 공약수
+    	
+    	
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		LeastCommonMultiple lCM = new LeastCommonMultiple();
+		int[] arr = {2, 6, 8, 14};
+		lCM.solution(arr);
+	}
+}
