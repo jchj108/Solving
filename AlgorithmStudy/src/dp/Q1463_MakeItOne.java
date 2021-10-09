e04099058b7e368f7a3434a1180fdc53caf1fda2
jchj108 <jchj108@naver.com>
2021-10-09 23:47:20 +0900

    1로 만들기

--- AlgorithmStudy/src/dp/Q1463_MakeItOne.java
+++ AlgorithmStudy/src/dp/Q1463_MakeItOne.java
@@ -1,7 +1,12 @@
 package dp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Q1463_MakeItOne {
 	public static void main(String[] args) {
-		//
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 	}
 }
