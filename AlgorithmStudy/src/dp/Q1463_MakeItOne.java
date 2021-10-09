9c48388133cad53dc1c53e53f84bd585305b8653
jchj108 <jchj108@naver.com>
2021-10-10 06:25:16 +0900

    피보나치 dp

--- AlgorithmStudy/src/dp/Q1463_MakeItOne.java
+++ AlgorithmStudy/src/dp/Q1463_MakeItOne.java
@@ -1,12 +1,28 @@
 package dp;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q1463_MakeItOne {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int N = Integer.parseInt(br.readLine());
+		
+		int count = dp(N);
+		
+		
+	}
+
+	private static int dp(int n) {
+		int count = 0;
+		if(n == 1) {
+			return count;
+		}
+		if(n % 3 == 0) {
+			return dp(n/3);
+		}
 	}
 }
---
+++ BaseAlgorithms/src/dp/Fibonacci.java
@@ -0,0 +1,26 @@
+package dp;
+
+public class Fibonacci {
+
+	static int[] d; // 메모리제이션
+
+	static int dp(int x) {
+		if (x == 1) {
+			return 1;
+		}
+		if (x == 2) {
+			return 1;
+		}
+		if (d[x] != 0) {
+			return d[x];
+		}
+		return d[x] = dp(x - 1) + dp(x - 2);
+	}
+
+	public static void main(String[] args) {
+		
+		d = new int[50];
+		
+		System.out.println(dp(11));
+	}
+}
