4739c4706c80c13b7d8290fac301d19a6460b010
ya <ya@DESKTOP-OQ78IB0>
2021-11-07 14:40:05 +0900

    virus(정답)

---
+++ AlgorithmStudy/src/dfsNBfs/Virus.java
@@ -0,0 +1,45 @@
+package dfsNBfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Virus {
+	
+	static int count = 0;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine()); // 정점(컴퓨터)의 개수
+		int m = Integer.parseInt(br.readLine()); // 간선의 개수
+		int v = 1; // 탐색을 시작할 컴퓨터 번호
+		
+		int[][] matrix = new int[n+1][n+1];
+		boolean[] visited = new boolean[n+1];
+		
+		for(int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			matrix[x][y] = 1;
+			matrix[y][x] = 1;
+		}
+		bfs(v, matrix, visited);
+		System.out.println(count - 1);
+	}
+
+	private static void bfs(int v, int[][] matrix, boolean[] visited) {
+		visited[v] = true;
+		count++;
+		
+		for(int i = 1; i < matrix.length; i++) {
+			if(matrix[v][i] == 1 && !visited[i]) { // 인접행렬 가로로 탐색, v는 재귀를 돌면서 바뀜
+				bfs(i, matrix, visited);
+			}
+		}
+		
+	}
+}
