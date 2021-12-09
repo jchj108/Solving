df7a85df0bbf39c7abc7679b74564590d6f39177
jchj108 <jchj108@naver.com>
2021-12-09 23:07:40 +0900

    연결요소의 개수 bfs 버전

--- AlgorithmStudy/src/dfsNBfs/CountConnectedComponent2.java
+++ AlgorithmStudy/src/dfsNBfs/CountConnectedComponent2.java
@@ -3,27 +3,28 @@ package dfsNBfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class CountConnectedComponent_11724 {
-	// 무방향 그래프
-
-	static int N, M;
+public class CountConnectedComponent2 { // 연결요소의 개수 찾기
+	// BFS 버전
+	static int N, M, count;
 	static int[][] matrix;
 	static boolean[] visited;
 	
-	public static void main(String args[]) throws IOException {
+	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken()); // 정점의 개수
+		
+		N = Integer.parseInt(st.nextToken()); // 정점의 개수 
 		M = Integer.parseInt(st.nextToken()); // 간선의 개수
-
-		matrix = new int[N + 1][N + 1];
+		
+		matrix = new int[N+1][N+1];
 		visited = new boolean[N+1];
 		
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < M; i++) { // 간선의 개수만큼 반복
 			st = new StringTokenizer(br.readLine());
 			int row = Integer.parseInt(st.nextToken());
 			int col = Integer.parseInt(st.nextToken());
@@ -31,24 +32,32 @@ public class CountConnectedComponent_11724 {
 			matrix[col][row] = 1;
 		}
 		
-		int count = 0;
-		
-		for(int i = 1; i <= N; i++) { // visited 배열 탐색하면서 dfs
+		for(int i = 1; i <= N; i++) {
 			if(!visited[i]) {
-				dfs(i);
+				bfs(i);
 				count++;
 			}
 		}
 		System.out.println(count);
+		
+		
 	}
 
-	private static void dfs(int start) {
-		visited[start] = true;
-		
-		for(int i = 1; i <= N; i++) {
-			if(matrix[start][i] == 1 && !visited[i]) {
-				dfs(i);
+	private static void bfs(int x) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(x);
+		visited[x] = true;
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			for(int i = 1; i <= N; i++) { 
+				if(!visited[i] && matrix[cur][i] == 1) {
+					visited[i] = true;
+					q.offer(i);
+				}
 			}
+			
 		}
+		
 	}
+
 }
