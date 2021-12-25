9c58e6a0500083a85367e9e686656faeb1423b1f
jchj108 <jchj108@naver.com>
2021-12-25 19:35:41 +0900

    숨박꼭질

---
+++ AlgorithmStudy/src/dfsNBfs/HideAndSeek_1697.java
@@ -0,0 +1,66 @@
+package dfsNBfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class HideAndSeek_1697 {
+
+	static int N;
+	static int K;
+	static int[] check = new int[1000001];
+	
+	public static void main(String[] args) throws IOException {
+		
+		//BFS 최단거리
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); // 수빈의 위치
+		int K = Integer.parseInt(st.nextToken()); // 동생의 위치
+		
+		if(N==K) {
+			System.out.println(0);
+		} else {
+			bfs(N);
+		}
+		
+		
+		
+	}
+
+	private static void bfs(int x) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(x);
+		check[x] = 1;
+		
+		while(!q.isEmpty()) {
+			int temp = q.poll();
+			
+			for(int i = 0; i < 3; i++) {
+				int next;
+				
+				if(i==0) {
+					next = temp + 1;
+				} else if (i==1) {
+					next = temp - 1;
+				} else {
+					next = temp * 2;
+				}
+				
+				if(next == K) {
+					System.out.println(check[temp]);
+					return;
+				}
+				if(next >= 0 && next < check.length && check[next] == 0) {
+					q.add(next);
+					check[next] = check[temp] + 1;
+				}
+			}			
+		}
+	}
+}
