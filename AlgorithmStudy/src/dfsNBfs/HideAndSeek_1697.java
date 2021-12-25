6d449822f26bac73af6c64578de3c5e022c07a15
jchj108 <jchj108@naver.com>
2021-12-25 19:43:26 +0900

    숨바꼭질

--- AlgorithmStudy/src/dfsNBfs/HideAndSeek_1697.java
+++ AlgorithmStudy/src/dfsNBfs/HideAndSeek_1697.java
@@ -20,47 +20,45 @@ public class HideAndSeek_1697 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken()); // 수빈의 위치
-		int K = Integer.parseInt(st.nextToken()); // 동생의 위치
+		N = Integer.parseInt(st.nextToken()); // 수빈의 위치
+		K = Integer.parseInt(st.nextToken()); // 동생의 위치
 		
 		if(N==K) {
 			System.out.println(0);
 		} else {
 			bfs(N);
 		}
-		
-		
-		
 	}
 
-	private static void bfs(int x) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(x);
-		check[x] = 1;
-		
-		while(!q.isEmpty()) {
-			int temp = q.poll();
-			
-			for(int i = 0; i < 3; i++) {
-				int next;
-				
-				if(i==0) {
-					next = temp + 1;
-				} else if (i==1) {
-					next = temp - 1;
-				} else {
-					next = temp * 2;
-				}
-				
-				if(next == K) {
-					System.out.println(check[temp]);
-					return;
-				}
-				if(next >= 0 && next < check.length && check[next] == 0) {
-					q.add(next);
-					check[next] = check[temp] + 1;
-				}
-			}			
-		}
-	}
+    static void bfs(int num) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(num);
+        check[num] = 1;
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int next;
+
+                if (i == 0) {
+                    next = temp + 1;
+                } else if (i == 1) {
+                    next = temp - 1;
+                } else {
+                    next = temp * 2;
+                }
+
+                if (next == K) {
+                    System.out.println(check[temp]);
+                    return;
+                }
+
+                if (next >= 0 && next < check.length && check[next] == 0) {
+                    q.add(next);
+                    check[next] = check[temp] + 1;
+                }
+            }
+        }
+    }
 }
