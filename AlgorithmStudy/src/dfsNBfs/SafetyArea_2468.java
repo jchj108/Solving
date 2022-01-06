3d1b283d256b35015c7b0b337107274f028f7e17
jchj108 <jchj108@naver.com>
2022-01-06 20:12:54 +0900

    안전영역

---
+++ AlgorithmStudy/src/dfsNBfs/SafetyArea_2468.java
@@ -0,0 +1,18 @@
+package dfsNBfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SafetyArea_2468 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		
+	}
+}
