e68c58e77de3dc81df823b785aa043d781ec5a31
jchj108 <jchj108@naver.com>
2022-03-28 01:24:28 +0900

    프로그래머스 h-index

---
+++ Programmers/src/level2/test/H_Index.java
@@ -0,0 +1,31 @@
+package level2.test;
+
+import java.util.Arrays;
+
+public class H_Index {
+
+	public static void main(String[] args) {
+		
+		int[] citations = {1, 3, 3};
+		
+		H_Index h = new H_Index();
+		
+		int result = h.solution(citations);
+		System.out.println(result);
+		
+		
+	}
+	public int solution(int[] citations) {
+		
+		Arrays.sort(citations);
+		
+		for(int i = 0; i < citations.length; i++) {
+			int h = citations.length-i; // count
+			
+			if(citations[i] >=h) {
+				return citations[h];
+			}
+		}
+		return 0;
+	}
+}
