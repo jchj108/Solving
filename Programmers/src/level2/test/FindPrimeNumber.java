ce77f59ea47ecd0f0bbb2fd16c3d82cf2616c21c
jchj108 <jchj108@naver.com>
2022-04-06 16:54:17 +0900

    완탐 소수찾기

--- Programmers/src/level2/test/FindPrimeNumber.java
+++ Programmers/src/level2/test/FindPrimeNumber.java
@@ -1,47 +1,68 @@
 package level2.test;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindPrimeNumber {
 
 	static boolean[] visited = new boolean[10];
-
+	static int count;
+	static Set<Integer> set = new HashSet<Integer>();
 	public static void main(String[] args) {
 
-		int result = solution("123");
-
+		int result = solution("17");
+		System.out.println(result);
 	}
 
 	public static int solution(String numbers) {
-		int answer = 0;
-		
 		String tmp = "";
-		for(int i = 0; i < numbers.length(); i++) {
-			dfs(numbers, tmp, i+1);
+		for (int i = 0; i < numbers.length(); i++) {
+			dfs(numbers, tmp, i + 1); // dfs depth 를 증가, depth가 1일 때는 1개만 뽑고 2일 때는 2개를 뽑는다.
 		}
-		
-		
 
-		return answer;
+		return count;
 	}
 
 	private static void dfs(String numbers, String tmp, int m) {
 		
-		if(tmp.length() == m) { 
-			System.out.println(tmp);
-			
+
+		if (tmp.length() == m) {
+			if(set.contains(Integer.parseInt(tmp))) {
+				return;
+			}
+			set.add(Integer.parseInt(tmp));
+			if (isPrime(tmp)) {
+				count++;
+			}
 			return;
 		}
-		
-		for(int i = 0; i < numbers.length(); i++) {
-			if(!visited[i]) {
+
+		for (int i = 0; i < numbers.length(); i++) {
+			if (!visited[i]) {
 				visited[i] = true;
 				tmp += numbers.charAt(i);
 				dfs(numbers, tmp, m);
 				visited[i] = false;
-				tmp = tmp.substring(0, tmp.length()-1);
+				tmp = tmp.substring(0, tmp.length() - 1); // 마지막에 고른 숫자 제거
 			}
 		}
+	}
+
+	private static boolean isPrime(String tmp) {
+		
+		int num = Integer.parseInt(tmp);
+		
+		if (num <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 }
