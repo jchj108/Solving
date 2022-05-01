7749fe259ed3b1a36f34b49efa2cedb1230ef197
jchj108 <jchj108@naver.com>
2022-05-01 22:57:41 +0900

    비숍, DNA 조합, 벽장문바꾸기

--- AlgorithmStudy/src/jungol/bfs/Bishop.java
+++ AlgorithmStudy/src/jungol/bfs/Bishop.java
@@ -6,48 +6,139 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Bishop {
-	static int N;
-	static int[][] map;
-	static int[][] position;
-	static boolean[][] visited;
+	// https://github.com/hotehrud/acmicpc
+	// http://mygumi.tistory.com/
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int[][] map, colors;
+	static boolean[] visited = new boolean[100];
+	static int n;
+	static int[] ans = new int[2];
+	static int[] dy = { -1, -1, 1, 1 };
+	static int[] dx = { -1, 1, 1, -1 };
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private void solve() {
+		n = sc.nextInt();
+		map = new int[n][n];
+		colors = new int[n][n];
 
-		N = Integer.parseInt(br.readLine().trim());
-
-		map = new int[N][N];
-		position = new int[N][N];
-		visited = new boolean[N][N];
-
-		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				map[i][j] = sc.nextInt();
+				if (i % 2 == 0) {
+					if (j % 2 == 0) {
+						colors[i][j] = 1;
+					}
+				} else {
+					if (j % 2 != 0) {
+						colors[i][j] = 1;
+					}
+				}
 			}
 		}
-		bfs(0, 0, 0);
+		dfs(-1, 0, 1);
+		dfs(-1, 0, 0);
+		System.out.println(ans[0] + ans[1]);
 	}
 
-
-	private static void bfs(int index, int j, int k) {
-		
-	}
-
-
-	private static void print() {
-		for (int i = 0; i < position.length; i++) {
-			for (int j = 0; j < position.length; j++) {
-				System.out.print(position[i][j]);
-			}
-			System.out.println();
+	public static void dfs(int v, int cnt, int color) {
+		if (ans[color] < cnt) {
+			ans[color] = cnt;
 		}
+
+		for (int i = v + 1; i < n * n; i++) {
+			int c = i / n;
+			int r = i % n;
+
+			if (colors[c][r] != color) {
+				continue;
+			}
+
+			if (map[c][r] == 1) {
+				if (check(c, r)) {
+					visited[i] = true;
+					dfs(i, cnt + 1, color);
+				}
+			}
+		}
+		if (v == -1) return;
+		visited[v] = false;
 	}
 
-	private static boolean isSafe() {
+	public static boolean check(int c, int r) {
+		for (int i = 0; i < 4; i++) {
+			int nx = dx[i] + r;
+			int ny = dy[i] + c;
+			int v = ny * n + nx;
 
+			for (int j = 1; j < n; j++) {
+				if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+					if (visited[v]) {
+						return false;
+					}
+				}
+				nx += dx[i];
+				ny += dy[i];
+				v = ny * n + nx;
+			}
+		}
 		return true;
+	}
+
+	public static void main(String[] args) {
+		sc.init();
+
+		new Bishop().solve();
+	}
+
+	static class sc {
+		private static BufferedReader br;
+		private static StringTokenizer st;
+
+		static void init() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
+
+		static String readLine() {
+			try {
+				return br.readLine();
+			} catch (IOException e) {
+			}
+			return null;
+		}
+
+		static String readLineReplace() {
+			try {
+				return br.readLine().replaceAll("\\s+", "");
+			} catch (IOException e) {
+			}
+			return null;
+		}
+
+		static String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+				}
+			}
+			return st.nextToken();
+		}
+
+		static boolean hasNext() {
+			return st.hasMoreTokens();
+		}
+
+		static long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		static int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		static double nextDouble() {
+			return Double.parseDouble(next());
+		}
 	}
 }
